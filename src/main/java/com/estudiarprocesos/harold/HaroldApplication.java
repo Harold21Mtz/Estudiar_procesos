1301f691de58e06128b13dfdabf139731aefc006
Harold21Mtz <hfmartinezc@ufpso.edu.co>
2022-10-05 16:26:07 -0500

    Initial commit

---
+++ src/main/java/com/estudiarprocesos/harold/HaroldApplication.java
@@ -0,0 +1,13 @@
+package com.estudiarprocesos.harold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaroldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaroldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudiarprocesos/harold/HaroldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudiarprocesos.harold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaroldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
